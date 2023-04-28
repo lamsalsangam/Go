61e310a5da2030b564b82bfcb24356c11dde800d
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-28 06:55:25 +0545

    Day 2 Go

---
+++ learn/Values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go has various value types including strings, integers, floats, booleans," + " etc.")
+	fmt.Println("")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("10.0/2.0 =", 10.0/2.0)
+	fmt.Println("")
+	fmt.Println("true && false =", true && false)
+	fmt.Println("true || false =", true || false)
+	fmt.Println("!true =", !true)
+}
