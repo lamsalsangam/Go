c9a7ab464b033310f22ef58201a62ccc30bb9333
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 16:24:44 +0545

    Omitting conditions in Go

--- learn/0. Another Path/Loops/main.go
+++ learn/0. Another Path/Loops/main.go
@@ -14,28 +14,58 @@ import (
 
 // ===============================
 
-func bulkSend(numMessages int) float64 {
+// func bulkSend(numMessages int) float64 {
+// 	// ?
+// 	totalCost := 0.0
+// 	for i := 0; i < numMessages; i++ {
+// 		totalCost += 1.0 + (0.01 * float64(i))
+// 	}
+// 	return totalCost
+// }
+
+// // don't edit below this line
+
+// func test(numMessages int) {
+// 	fmt.Printf("Sending %v messages\n", numMessages)
+// 	cost := bulkSend(numMessages)
+// 	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+// 	fmt.Println("===============================================================")
+// }
+
+// func main() {
+// 	test(10)
+// 	test(20)
+// 	test(30)
+// 	test(40)
+// 	test(50)
+// }
+
+// ################################################################
+// Omitting Conditions
+func maxMessages(thresh float64) int {
 	// ?
 	totalCost := 0.0
-	for i := 0; i < numMessages; i++ {
+	for i := 0; ; i++ {
 		totalCost += 1.0 + (0.01 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
 	}
-	return totalCost
 }
 
 // don't edit below this line
 
-func test(numMessages int) {
-	fmt.Printf("Sending %v messages\n", numMessages)
-	cost := bulkSend(numMessages)
-	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+func test(thresh float64) {
+	fmt.Printf("Threshold: %.2f\n", thresh)
+	max := maxMessages(thresh)
+	fmt.Printf("Maximum messages that can be sent: = %v\n", max)
 	fmt.Println("===============================================================")
 }
 
 func main() {
-	test(10)
-	test(20)
-	test(30)
-	test(40)
-	test(50)
+	test(10.00)
+	test(20.00)
+	test(30.00)
+	test(40.00)
+	test(50.00)
 }
--- learn/0. Another Path/Readme.md
+++ learn/0. Another Path/Readme.md
@@ -5,3 +5,4 @@
 3. [Structs](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Structs)
 4. [Interface](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Interface)
 5. [Error](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Errors)
+6. [Loops](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Loops)
