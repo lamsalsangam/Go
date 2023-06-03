f91f08080c79c435c881234e094ac36f426bb40c
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 15:29:27 +0545

    The Error Interface

--- learn/0. Another Path/Errors/main.go
+++ learn/0. Another Path/Errors/main.go
@@ -95,24 +95,63 @@ import "fmt"
 
 // ################################################################
 
-func getSMSErrorString(cost float64, recipient string) string {
-	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent
-	",cost,recipient)
+// func getSMSErrorString(cost float64, recipient string) string {
+// 	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent
+// 	",cost,recipient)
+// }
+
+// // don't edit below this line
+
+// func test(cost float64, recipient string) {
+// 	s := getSMSErrorString(cost, recipient)
+// 	fmt.Println(s)
+// 	fmt.Println("====================================")
+// }
+
+// func main() {
+// 	test(1.4, "+1 (435) 555 0923")
+// 	test(2.1, "+2 (702) 555 3452")
+// 	test(32.1, "+1 (801) 555 7456")
+// 	test(14.4, "+1 (234) 555 6545")
+// }
+
+// ################################################################
+
+type divideError struct {
+	dividend float64
+}
+
+// ?
+func (de divideError) Error() string {
+	return fmt.Sprintf("cannot divide %v by zero", de.dividend)
 }
 
 // don't edit below this line
 
-func test(cost float64, recipient string) {
-	s := getSMSErrorString(cost, recipient)
-	fmt.Println(s)
-	fmt.Println("====================================")
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		// We convert the `divideError` struct to an `error` type by returning it
+		// as an error. As an error type, when it's printed its default value
+		// will be the result of the Error() method
+		return 0, divideError{dividend: dividend}
+	}
+	return dividend / divisor, nil
+}
+
+func test(dividend, divisor float64) {
+	defer fmt.Println("====================================")
+	fmt.Printf("Dividing %.2f by %.2f ...\n", dividend, divisor)
+	quotient, err := divide(dividend, divisor)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Quotient: %.2f\n", quotient)
 }
 
 func main() {
-	test(1.4, "+1 (435) 555 0923")
-	test(2.1, "+2 (702) 555 3452")
-	test(32.1, "+1 (801) 555 7456")
-	test(14.4, "+1 (234) 555 6545")
+	test(10, 0)
+	test(10, 2)
+	test(15, 30)
+	test(6, 3)
 }
-
-// ################################################################
--- learn/0. Another Path/Readme.md
+++ learn/0. Another Path/Readme.md
@@ -4,3 +4,4 @@
 2. [Functions](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Functions)
 3. [Structs](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Structs)
 4. [Interface](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Interface)
+5. [Error](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Errors)
