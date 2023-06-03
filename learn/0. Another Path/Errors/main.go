78319cda77c1dca75d24bdfaada4c67c9a41a9f6
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 15:40:08 +0545

    The Error Package

--- learn/0. Another Path/Errors/main.go
+++ learn/0. Another Path/Errors/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // ################################################################
 // type error interface {
@@ -116,32 +119,63 @@ import "fmt"
 // }
 
 // ################################################################
+// The Error interface
 
-type divideError struct {
-	dividend float64
-}
+// type divideError struct {
+// 	dividend float64
+// }
 
-// ?
-func (de divideError) Error() string {
-	return fmt.Sprintf("cannot divide %v by zero", de.dividend)
+// // ?
+// func (de divideError) Error() string {
+// 	return fmt.Sprintf("cannot divide %v by zero", de.dividend)
+// }
+
+// // don't edit below this line
+
+// func divide(dividend, divisor float64) (float64, error) {
+// 	if divisor == 0 {
+// 		// We convert the `divideError` struct to an `error` type by returning it
+// 		// as an error. As an error type, when it's printed its default value
+// 		// will be the result of the Error() method
+// 		return 0, divideError{dividend: dividend}
+// 	}
+// 	return dividend / divisor, nil
+// }
+
+// func test(dividend, divisor float64) {
+// 	defer fmt.Println("====================================")
+// 	fmt.Printf("Dividing %.2f by %.2f ...\n", dividend, divisor)
+// 	quotient, err := divide(dividend, divisor)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Printf("Quotient: %.2f\n", quotient)
+// }
+
+// func main() {
+// 	test(10, 0)
+// 	test(10, 2)
+// 	test(15, 30)
+// 	test(6, 3)
+// }
+
+// ################################################################
+// The Error package
+func divide(x, y float64) (float64, error) {
+	if y == 0 {
+		// ?
+		return 0.0, errors.New("No didviding by 0")
+	}
+	return x / y, nil
 }
 
 // don't edit below this line
 
-func divide(dividend, divisor float64) (float64, error) {
-	if divisor == 0 {
-		// We convert the `divideError` struct to an `error` type by returning it
-		// as an error. As an error type, when it's printed its default value
-		// will be the result of the Error() method
-		return 0, divideError{dividend: dividend}
-	}
-	return dividend / divisor, nil
-}
-
-func test(dividend, divisor float64) {
+func test(x, y float64) {
 	defer fmt.Println("====================================")
-	fmt.Printf("Dividing %.2f by %.2f ...\n", dividend, divisor)
-	quotient, err := divide(dividend, divisor)
+	fmt.Printf("Dividing %.2f by %.2f ...\n", x, y)
+	quotient, err := divide(x, y)
 	if err != nil {
 		fmt.Println(err)
 		return
