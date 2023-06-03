1df7db6b54ddf8e9e5b99a762243e746d8f03d23
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 10:58:14 +0545

    Type Assertion

--- learn/0. Another Path/Interface/main.go
+++ learn/0. Another Path/Interface/main.go
@@ -151,63 +151,185 @@ import (
 // ################################################################
 // Multiple Interface
 
-func (e email) cost() float64 {
-	// ?
-	if !e.isSubscribed {
-		return 0.05 * float64(len(e.body))
-	}
-	return 0.01 * float64(len(e.body))
-}
+// func (e email) cost() float64 {
+// 	// ?
+// 	if !e.isSubscribed {
+// 		return 0.05 * float64(len(e.body))
+// 	}
+// 	return 0.01 * float64(len(e.body))
+// }
 
-func (e email) print() {
-	// ?
-	fmt.Println(e.body)
+// func (e email) print() {
+// 	// ?
+// 	fmt.Println(e.body)
+// }
+
+// // don't touch below this line
+
+// type expense interface {
+// 	cost() float64
+// }
+
+// type printer interface {
+// 	print()
+// }
+
+// type email struct {
+// 	isSubscribed bool
+// 	body         string
+// }
+
+// func print(p printer) {
+// 	p.print()
+// }
+
+// func test(e expense, p printer) {
+// 	fmt.Printf("Printing with cost: $%.2f ...\n", e.cost())
+// 	p.print()
+// 	fmt.Println("====================================")
+// }
+
+// func main() {
+// 	e := email{
+// 		isSubscribed: true,
+// 		body:         "hello there",
+// 	}
+// 	test(e, e)
+// 	e = email{
+// 		isSubscribed: false,
+// 		body:         "I want my money back",
+// 	}
+// 	test(e, e)
+// 	e = email{
+// 		isSubscribed: true,
+// 		body:         "Are you free for a chat?",
+// 	}
+// 	test(e, e)
+// 	e = email{
+// 		isSubscribed: false,
+// 		body:         "This meeting could have been an email",
+// 	}
+// 	test(e, e)
+// }
+
+// ################################################################
+// Type Assertion
+// type shape interface {
+// 	area() float64
+// }
+
+// type circle struct {
+// 	radius float64
+// }
+
+// // "c" is a new circle cast from "s"
+// // which is an instance of a shape.
+// // "ok" is a bool that is true if s was a circle
+// // or false if s isn't a circle
+// c, ok := s.(circle)
+// if !ok {
+// 	// s wasn't a circle
+// 	log.Fatal("s is not a circle")
+// }
+
+// radius := c.radius
+
+// ===============================
+
+func getExpenseReport(e expense) (string, float64) {
+	em, ok := e.(email)
+	if ok {
+		return em.toAddress, em.cost()
+	}
+	s, ok := e.(sms)
+	if ok {
+		return s.toPhoneNumber, s.cost()
+	}
+	return "", 0.0
 }
 
 // don't touch below this line
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
 
 type expense interface {
 	cost() float64
 }
 
-type printer interface {
-	print()
-}
-
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
 }
 
-func print(p printer) {
-	p.print()
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
 }
 
-func test(e expense, p printer) {
-	fmt.Printf("Printing with cost: $%.2f ...\n", e.cost())
-	p.print()
-	fmt.Println("====================================")
+type invalid struct{}
+
+func estimateYearlyCost(e expense, averageMessagesPerYear int) float64 {
+	return e.cost() * float64(averageMessagesPerYear)
+}
+
+func test(e expense) {
+	address, cost := getExpenseReport(e)
+	switch e.(type) {
+	case email:
+		fmt.Printf("Report: The email going to %s will cost: %.2f\n", address, cost)
+		fmt.Println("====================================")
+	case sms:
+		fmt.Printf("Report: The sms going to %s will cost: %.2f\n", address, cost)
+		fmt.Println("====================================")
+	default:
+		fmt.Println("Report: Invalid expense")
+		fmt.Println("====================================")
+	}
 }
 
 func main() {
-	e := email{
+	test(email{
 		isSubscribed: true,
 		body:         "hello there",
-	}
-	test(e, e)
-	e = email{
-		isSubscribed: false,
-		body:         "I want my money back",
-	}
-	test(e, e)
-	e = email{
-		isSubscribed: true,
-		body:         "Are you free for a chat?",
-	}
-	test(e, e)
-	e = email{
+		toAddress:    "john@does.com",
+	})
+	test(email{
 		isSubscribed: false,
 		body:         "This meeting could have been an email",
-	}
-	test(e, e)
+		toAddress:    "jane@doe.com",
+	})
+	test(email{
+		isSubscribed: false,
+		body:         "This meeting could have been an email",
+		toAddress:    "elon@doe.com",
+	})
+	test(sms{
+		isSubscribed:  false,
+		body:          "This meeting could have been an email",
+		toPhoneNumber: "+155555509832",
+	})
+	test(sms{
+		isSubscribed:  false,
+		body:          "This meeting could have been an email",
+		toPhoneNumber: "+155555504444",
+	})
+	test(invalid{})
 }
