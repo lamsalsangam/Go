b8abb7cdda9d3293d6b1bf1f3121a9333632ba78
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 15:23:01 +0545

    Formatting String Review

--- learn/0. Another Path/Errors/main.go
+++ learn/0. Another Path/Errors/main.go
@@ -39,56 +39,80 @@ import "fmt"
 
 // }
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error) {
-	costForCustomer, err := sendSMS(msgToCustomer)
-	if err != nil {
-		return 0.0, err
-	}
-	costForSpouse, err := sendSMS(msgToSpouse)
-	if err != nil {
-		return 0.0, err
-	}
-	return costForCustomer + costForSpouse, nil
+// func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error) {
+// 	costForCustomer, err := sendSMS(msgToCustomer)
+// 	if err != nil {
+// 		return 0.0, err
+// 	}
+// 	costForSpouse, err := sendSMS(msgToSpouse)
+// 	if err != nil {
+// 		return 0.0, err
+// 	}
+// 	return costForCustomer + costForSpouse, nil
+// }
+
+// // don't edit below this line
+
+// func sendSMS(message string) (float64, error) {
+// 	const maxTextLen = 25
+// 	const costPerChar = .0002
+// 	if len(message) > maxTextLen {
+// 		return 0.0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+// 	}
+// 	return costPerChar * float64(len(message)), nil
+// }
+
+// func test(msgToCustomer, msgToSpouse string) {
+// 	defer fmt.Println("========")
+// 	fmt.Println("Message for customer:", msgToCustomer)
+// 	fmt.Println("Message for spouse:", msgToSpouse)
+// 	totalCost, err := sendSMSToCouple(msgToCustomer, msgToSpouse)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+// 	fmt.Printf("Total cost: $%.4f\n", totalCost)
+// }
+
+// func main() {
+// 	test(
+// 		"Thanks for coming in to our flower shop today!",
+// 		"We hope you enjoyed your gift.",
+// 	)
+// 	test(
+// 		"Thanks for joining us!",
+// 		"Have a good day.",
+// 	)
+// 	test(
+// 		"Thank you.",
+// 		"Enjoy!",
+// 	)
+// 	test(
+// 		"We loved having you in!",
+// 		"We hope the rest of your evening is absolutely fantastic.",
+// 	)
+// }
+
+// ################################################################
+
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent
+	",cost,recipient)
 }
 
 // don't edit below this line
 
-func sendSMS(message string) (float64, error) {
-	const maxTextLen = 25
-	const costPerChar = .0002
-	if len(message) > maxTextLen {
-		return 0.0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
-	}
-	return costPerChar * float64(len(message)), nil
-}
-
-func test(msgToCustomer, msgToSpouse string) {
-	defer fmt.Println("========")
-	fmt.Println("Message for customer:", msgToCustomer)
-	fmt.Println("Message for spouse:", msgToSpouse)
-	totalCost, err := sendSMSToCouple(msgToCustomer, msgToSpouse)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Printf("Total cost: $%.4f\n", totalCost)
+func test(cost float64, recipient string) {
+	s := getSMSErrorString(cost, recipient)
+	fmt.Println(s)
+	fmt.Println("====================================")
 }
 
 func main() {
-	test(
-		"Thanks for coming in to our flower shop today!",
-		"We hope you enjoyed your gift.",
-	)
-	test(
-		"Thanks for joining us!",
-		"Have a good day.",
-	)
-	test(
-		"Thank you.",
-		"Enjoy!",
-	)
-	test(
-		"We loved having you in!",
-		"We hope the rest of your evening is absolutely fantastic.",
-	)
+	test(1.4, "+1 (435) 555 0923")
+	test(2.1, "+2 (702) 555 3452")
+	test(32.1, "+1 (801) 555 7456")
+	test(14.4, "+1 (234) 555 6545")
 }
+
+// ################################################################
