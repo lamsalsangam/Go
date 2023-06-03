a73c1478059a1668016952c39790df0e6aac90dc
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-03 06:32:58 +0545

    Named Return values

--- learn/0. Another Path/Functions/main.go
+++ learn/0. Another Path/Functions/main.go
@@ -2,12 +2,65 @@ package main
 
 import "fmt"
 
-func concat(s1 string, s2 string) string {
-	return s1 + s2
+// func concat(s1 string, s2 string) string {
+// 	return s1 + s2
+// }
+
+// func concat(s1, s2 string) string {
+// 	return s1 + s2
+// }
+
+// func main() {
+// 	fmt.Println("XXXXXXX", " Happy Birthday")
+// 	fmt.Println("Sangam", " Rich")
+// 	fmt.Println("Go", " is Fantastic")
+// }
+
+// #################################################################
+
+// func main() {
+// 	sendsSoFar := 430
+// 	const sendsToAdd = 25
+// 	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
+// 	fmt.Println("You've sent", sendsSoFar, "message")
+// }
+
+// func incrementSends(sendsSoFar, sendsToAdd int) int {
+// 	sendsSoFar = sendsSoFar + sendsToAdd
+// 	return sendsSoFar
+// }
+
+// #################################################################
+// Named Return values
+func yearsUntilEvents(age int) (
+	yearsUntilAdult,
+	yearsUntilDrinking,
+	yearsUntilCarRental int) {
+
+	yearsUntilAdult = 18 - age
+	if yearsUntilAdult < 0 {
+		yearsUntilAdult = 0
+	}
+	if yearsUntilDrinking = 21 - age; yearsUntilDrinking < 0 {
+		yearsUntilDrinking = 0
+	}
+	if yearsUntilCarRental = 25 - age; yearsUntilCarRental < 0 {
+		yearsUntilCarRental = 0
+	}
+	return
+}
+
+func test(age int) {
+	fmt.Println("Age", age)
+	yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental := yearsUntilEvents(age)
+	fmt.Println("You are an adult in", yearsUntilAdult, "years")
+	fmt.Println("You can drink in", yearsUntilDrinking, "years")
+	fmt.Println("You can rent a car in", yearsUntilCarRental, "years")
+	fmt.Println("===================================================")
 }
 
 func main() {
-	fmt.Println("XXXXXXX", " Happy Birthday")
-	fmt.Println("Sangam", " Rich")
-	fmt.Println("Go", " is Fantastic")
+	test(4)
+	test(10)
+	test(22)
 }
