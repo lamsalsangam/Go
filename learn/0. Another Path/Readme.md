72bfc617869dd8a653e98fd801a0a98de7d920d9
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-05 06:35:04 +0545

    Slices Review

--- learn/0. Another Path/Readme.md
+++ learn/0. Another Path/Readme.md
@@ -6,3 +6,4 @@
 4. [Interface](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Interface)
 5. [Error](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Errors)
 6. [Loops](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Loops)
+7. [Slices](https://github.com/lamsalsangam/Go/tree/master/learn/0.%20Another%20Path/Slices)
--- learn/0. Another Path/Slices/main.go
+++ learn/0. Another Path/Slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // To declare an array of 10 integers:
 
@@ -10,28 +13,95 @@ import "fmt"
 
 // primes := [6]int{2, 3, 5, 7, 11, 13}
 
+// to declare the slices
+
+// mySlice := primes[1:4]
+
+// ===============================
+
+// const (
+// 	retry1 = "click here to sign up"
+// 	retry2 = "pretty please click here"
+// 	retry3 = "we beg you to sign up"
+// )
+
+// func getMessageWithRetries() [3]string {
+// 	// ?
+// 	return [3]string{
+// 		retry1,
+// 		retry2,
+// 		retry3,
+// 	}
+// }
+
+// // don't touch below this line
+
+// func testSend(name string, doneAt int) {
+// 	fmt.Printf("sending to %v...", name)
+// 	fmt.Println()
+
+// 	messages := getMessageWithRetries()
+// 	for i := 0; i < len(messages); i++ {
+// 		msg := messages[i]
+// 		fmt.Printf(`sending: "%v"`, msg)
+// 		fmt.Println()
+// 		if i == doneAt {
+// 			fmt.Println("they responded!")
+// 			break
+// 		}
+// 		if i == len(messages)-1 {
+// 			fmt.Println("complete failure")
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	testSend("Bob", 0)
+// 	testSend("Alice", 1)
+// 	testSend("Mangalam", 2)
+// 	testSend("Ozgur", 3)
+// }
+
+// ################################################################
+// Slices Review
+
 const (
-	retry1 = "click here to sign up"
-	retry2 = "pretty please click here"
-	retry3 = "we beg you to sign up"
+	planFree = "free"
+	planPro  = "pro"
 )
 
-func getMessageWithRetries() [3]string {
+func getMessageWithRetriesForPlan(plan string) ([]string, error) {
+	allMessages := getMessageWithRetries()
 	// ?
-	return [3]string{
-		retry1,
-		retry2,
-		retry3,
+	if plan == planPro {
+		return allMessages[:], nil
 	}
+	if plan == planFree {
+		return allMessages[:2], nil
+	}
+	return nil, errors.New("unsupported plan")
 }
 
 // don't touch below this line
 
-func testSend(name string, doneAt int) {
+func getMessageWithRetries() [3]string {
+	return [3]string{
+		"click here to sign up",
+		"pretty please click here",
+		"we beg you to sign up",
+	}
+}
+
+func test(name string, doneAt int, plan string) {
+	defer fmt.Println("=====================================")
 	fmt.Printf("sending to %v...", name)
 	fmt.Println()
 
-	messages := getMessageWithRetries()
+	messages, err := getMessageWithRetriesForPlan(plan)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 	for i := 0; i < len(messages); i++ {
 		msg := messages[i]
 		fmt.Printf(`sending: "%v"`, msg)
@@ -41,14 +111,14 @@ func testSend(name string, doneAt int) {
 			break
 		}
 		if i == len(messages)-1 {
-			fmt.Println("complete failure")
+			fmt.Println("no response")
 		}
 	}
 }
 
 func main() {
-	testSend("Bob", 0)
-	testSend("Alice", 1)
-	testSend("Mangalam", 2)
-	testSend("Ozgur", 3)
+	test("Ozgur", 3, planFree)
+	test("Jeff", 3, planPro)
+	test("Sally", 2, planPro)
+	test("Sally", 3, "no plan")
 }
