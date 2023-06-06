703a103e32ec5af16330cc78697398cea1c17400
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-06 05:50:39 +0545

    Slices Append

--- learn/0. Another Path/Slices/main.go
+++ learn/0. Another Path/Slices/main.go
@@ -133,54 +133,178 @@ import (
 // the capacity argument is usually omitted and defaults to the length
 // mySlice := make([]int, 5)
 
-func getMessageCosts(messages []string) []float64 {
-	// ?
-	costs := make([]float64, len(messages))
-	for i := 0; i < len(messages); i++ {
-		message := messages[i]
-		cost := float64(len(message)) * 0.01
-		costs[i] = cost
-	}
-	return costs
+// ===============================
+
+// func getMessageCosts(messages []string) []float64 {
+// 	// ?
+// 	costs := make([]float64, len(messages))
+// 	for i := 0; i < len(messages); i++ {
+// 		message := messages[i]
+// 		cost := float64(len(message)) * 0.01
+// 		costs[i] = cost
+// 	}
+// 	return costs
+// }
+
+// // don't edit below this line
+
+// func test(messages []string) {
+// 	costs := getMessageCosts(messages)
+// 	fmt.Println("Messages:")
+// 	for i := 0; i < len(messages); i++ {
+// 		fmt.Printf(" - %v\n", messages[i])
+// 	}
+// 	fmt.Println("Costs:")
+// 	for i := 0; i < len(costs); i++ {
+// 		fmt.Printf(" - %.2f\n", costs[i])
+// 	}
+// 	fmt.Println("===== END REPORT =====")
+// }
+
+// func main() {
+// 	test([]string{
+// 		"Welcome to the movies!",
+// 		"Enjoy your popcorn!",
+// 		"Please don't talk during the movie!",
+// 	})
+// 	test([]string{
+// 		"I don't want to be here anymore",
+// 		"Can we go home?",
+// 		"I'm hungry",
+// 		"I'm bored",
+// 	})
+// 	test([]string{
+// 		"Hello",
+// 		"Hi",
+// 		"Hey",
+// 		"Hi there",
+// 		"Hey there",
+// 		"Hi there",
+// 		"Hello there",
+// 		"Hey there",
+// 		"Hello there",
+// 		"General Kenobi",
+// 	})
+// }
+
+// ################################################################
+// Variadic
+
+// Many functions, especially those in the standard library, can take an arbitrary number of final arguments. This is accomplished by using the "..." syntax in the function signature.
+
+// A variadic function receives the variadic arguments as a slice.
+
+// func concat(strs ...string) string {
+//     final := ""
+//     // strs is just a slice of strings
+//     for _, str := range strs {
+//         final += str
+//     }
+//     return final
+// }
+
+// func main() {
+//     final := concat("Hello ", "there ", "friend!")
+//     fmt.Println(total)
+//     // Output: Hello there friend!
+// }
+
+// ===============================
+
+// func sum(nums ...float64) float64 {
+// 	// ?
+// 	total := 0.0
+// 	for i := 0; i < len(nums); i++ {
+// 		total += nums[i]
+// 	}
+// 	return total
+// }
+
+// // don't edit below this line
+
+// func test(nums ...float64) {
+// 	total := sum(nums...)
+// 	fmt.Printf("Summing %v costs...\n", len(nums))
+// 	fmt.Printf("Bill for the month: %.2f\n", total)
+// 	fmt.Println("===== END REPORT =====")
+// }
+
+// func main() {
+// 	test(1.0, 2.0, 3.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
+// 	test(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0)
+// }
+
+// ################################################################
+// Append
+
+// The built-in append function is used to dynamically add elements to a slice:
+
+// func append(slice []Type, elems ...Type) []Type
+// If the underlying array is not large enough, append() will create a new underlying array and point the slice to it.
+
+// Notice that append() is variadic, the following are all valid:
+
+// slice = append(slice, oneThing)
+// slice = append(slice, firstThing, secondThing)
+// slice = append(slice, anotherSlice...)
+
+// ===============================
+
+type cost struct {
+	day   int
+	value float64
 }
 
-// don't edit below this line
-
-func test(messages []string) {
-	costs := getMessageCosts(messages)
-	fmt.Println("Messages:")
-	for i := 0; i < len(messages); i++ {
-		fmt.Printf(" - %v\n", messages[i])
-	}
-	fmt.Println("Costs:")
+func getCostsByDay(costs []cost) []float64 {
+	// ?
+	costsByDay := []float64{}
 	for i := 0; i < len(costs); i++ {
-		fmt.Printf(" - %.2f\n", costs[i])
+		cost := costs[i]
+		for cost.day >= len(costsByDay) {
+			costsByDay = append(costsByDay, 0.0)
+		}
+		costsByDay[cost.day] += cost.value
+	}
+	return costsByDay
+}
+
+// dont edit below this line
+
+func test(costs []cost) {
+	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
+	costsByDay := getCostsByDay(costs)
+	fmt.Println("Costs by day:")
+	for i := 0; i < len(costsByDay); i++ {
+		fmt.Printf(" - Day %v: %.2f\n", i, costsByDay[i])
 	}
 	fmt.Println("===== END REPORT =====")
 }
 
 func main() {
-	test([]string{
-		"Welcome to the movies!",
-		"Enjoy your popcorn!",
-		"Please don't talk during the movie!",
+	test([]cost{
+		{0, 1.0},
+		{1, 2.0},
+		{1, 3.1},
+		{2, 2.5},
+		{3, 3.6},
+		{3, 2.7},
+		{4, 3.34},
 	})
-	test([]string{
-		"I don't want to be here anymore",
-		"Can we go home?",
-		"I'm hungry",
-		"I'm bored",
-	})
-	test([]string{
-		"Hello",
-		"Hi",
-		"Hey",
-		"Hi there",
-		"Hey there",
-		"Hi there",
-		"Hello there",
-		"Hey there",
-		"Hello there",
-		"General Kenobi",
+	test([]cost{
+		{0, 1.0},
+		{10, 2.0},
+		{3, 3.1},
+		{2, 2.5},
+		{1, 3.6},
+		{2, 2.7},
+		{4, 56.34},
+		{13, 2.34},
+		{28, 1.34},
+		{25, 2.34},
+		{30, 4.34},
 	})
 }
+
+// ################################################################
