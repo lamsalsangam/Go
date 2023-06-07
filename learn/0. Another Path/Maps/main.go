c7aeae0d2fbca6f370d49b734e3c3a7f7642532a
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-07 10:40:09 +0545

    Count Instances

--- learn/0. Another Path/Maps/main.go
+++ learn/0. Another Path/Maps/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"errors"
+	"crypto/md5"
 	"fmt"
-	"sort"
+	"io"
 )
 
 // ################################################################
@@ -102,78 +102,156 @@ import (
 
 // ===============================
 
-func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+// func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+// 	// ?
+// 	existingUser, ok := users[name]
+// 	if !ok {
+// 		return false, errors.New("not found")
+// 	}
+// 	if existingUser.scheduledForDeletion {
+// 		delete(users, name)
+// 		return true, nil
+// 	}
+// 	return false, nil
+// }
+
+// // don't touch below this line
+
+// type user struct {
+// 	name                 string
+// 	number               int
+// 	scheduledForDeletion bool
+// }
+
+// func test(users map[string]user, name string) {
+// 	fmt.Printf("Attempting to delete %s...\n", name)
+// 	defer fmt.Println("====================================")
+// 	deleted, err := deleteIfNecessary(users, name)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	if deleted {
+// 		fmt.Println("Deleted:", name)
+// 		return
+// 	}
+// 	fmt.Println("Did not delete:", name)
+// }
+
+// func main() {
+// 	users := map[string]user{
+// 		"john": {
+// 			name:                 "john",
+// 			number:               18965554631,
+// 			scheduledForDeletion: true,
+// 		},
+// 		"elon": {
+// 			name:                 "elon",
+// 			number:               19875556452,
+// 			scheduledForDeletion: true,
+// 		},
+// 		"breanna": {
+// 			name:                 "breanna",
+// 			number:               98575554231,
+// 			scheduledForDeletion: false,
+// 		},
+// 		"kade": {
+// 			name:                 "kade",
+// 			number:               10765557221,
+// 			scheduledForDeletion: false,
+// 		},
+// 	}
+// 	test(users, "john")
+// 	test(users, "musk")
+// 	test(users, "santa")
+// 	test(users, "kade")
+
+// 	keys := []string{}
+// 	for name := range users {
+// 		keys = append(keys, name)
+// 	}
+// 	sort.Strings(keys)
+
+// 	fmt.Println("Final map keys:")
+// 	for _, name := range keys {
+// 		fmt.Println(" - ", name)
+// 	}
+// }
+
+// ################################################################
+
+// KEY TYPES
+// Any type can be used as the value in a map, but keys are more restrictive.
+
+// As mentioned earlier, map keys may be of any type that is comparable. The language spec defines this precisely, but in short, comparable types are boolean, numeric, string, pointer, channel, and interface types, and structs or arrays that contain only those types. Notably absent from the list are slices, maps, and functions; these types cannot be compared using ==, and may not be used as map keys.
+
+// It's obvious that strings, ints, and other basic types should be available as map keys, but perhaps unexpected are struct keys. Struct can be used to key data by multiple dimensions. For example, this map of maps could be used to tally web page hits by country:
+
+// hits := make(map[string]map[string]int)
+// This is map of string to (map of string to int). Each key of the outer map is the path to a web page with its own inner map. Each inner map key is a two-letter country code. This expression retrieves the number of times an Australian has loaded the documentation page:
+
+// n := hits["/doc/"]["au"]
+// Unfortunately, this approach becomes unwieldy when adding data, as for any given outer key you must check if the inner map exists, and create it if needed:
+
+// func add(m map[string]map[string]int, path, country string) {
+//     mm, ok := m[path]
+//     if !ok {
+//         mm = make(map[string]int)
+//         m[path] = mm
+//     }
+//     mm[country]++
+// }
+// add(hits, "/doc/", "au")
+// On the other hand, a design that uses a single map with a struct key does away with all that complexity:
+
+// type Key struct {
+//     Path, Country string
+// }
+// hits := make(map[Key]int)
+// When a Vietnamese person visits the home page, incrementing (and possibly creating) the appropriate counter is a one-liner:
+
+// hits[Key{"/", "vn"}]++
+// And it’s similarly straightforward to see how many Swiss people have read the spec:
+
+// n := hits[Key{"/ref/spec", "ch"}]
+
+// ===============================
+
+func getCounts(userIDs []string) map[string]int {
 	// ?
-	existingUser, ok := users[name]
-	if !ok {
-		return false, errors.New("not found")
+	counts := make(map[string]int)
+	for _, userID := range userIDs {
+		count := counts[userID]
+		count++
+		counts[userID] = count
 	}
-	if existingUser.scheduledForDeletion {
-		delete(users, name)
-		return true, nil
-	}
-	return false, nil
+	return counts
 }
 
-// don't touch below this line
+// don't edit below this line
 
-type user struct {
-	name                 string
-	number               int
-	scheduledForDeletion bool
-}
+func test(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
 
-func test(users map[string]user, name string) {
-	fmt.Printf("Attempting to delete %s...\n", name)
-	defer fmt.Println("====================================")
-	deleted, err := deleteIfNecessary(users, name)
-	if err != nil {
-		fmt.Println(err)
-		return
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
 	}
-	if deleted {
-		fmt.Println("Deleted:", name)
-		return
-	}
-	fmt.Println("Did not delete:", name)
+	fmt.Println("=====================================")
 }
 
 func main() {
-	users := map[string]user{
-		"john": {
-			name:                 "john",
-			number:               18965554631,
-			scheduledForDeletion: true,
-		},
-		"elon": {
-			name:                 "elon",
-			number:               19875556452,
-			scheduledForDeletion: true,
-		},
-		"breanna": {
-			name:                 "breanna",
-			number:               98575554231,
-			scheduledForDeletion: false,
-		},
-		"kade": {
-			name:                 "kade",
-			number:               10765557221,
-			scheduledForDeletion: false,
-		},
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
 	}
-	test(users, "john")
-	test(users, "musk")
-	test(users, "santa")
-	test(users, "kade")
 
-	keys := []string{}
-	for name := range users {
-		keys = append(keys, name)
-	}
-	sort.Strings(keys)
-
-	fmt.Println("Final map keys:")
-	for _, name := range keys {
-		fmt.Println(" - ", name)
-	}
+	test(userIDs, []string{"00", "ff", "dd"})
+	test(userIDs, []string{"aa", "12", "32"})
+	test(userIDs, []string{"bb", "33"})
 }
