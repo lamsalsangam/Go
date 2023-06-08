ccab65e93619e499c1139537450a0fa28ec4db1c
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-09 04:57:19 +0545

    Anonymous functions

--- learn/0. Another Path/Advanced Function/main.go
+++ learn/0. Another Path/Advanced Function/main.go
@@ -258,50 +258,121 @@ import (
 
 // ===============================
 
-func adder() func(int) int {
+// func adder() func(int) int {
+// 	// ?
+// 	sum := 0
+// 	return func(i int) int {
+// 		sum += i
+// 		return sum
+// 	}
+// }
+
+// // don't touch below this line
+
+// type emailBill struct {
+// 	costInPennies int
+// }
+
+// func test(bills []emailBill) {
+// 	defer fmt.Println("====================================")
+// 	countAdder, costAdder := adder(), adder()
+// 	for _, bill := range bills {
+// 		fmt.Printf("You've sent %d emails and it has cost you %d cents\n", countAdder(1), costAdder(bill.costInPennies))
+// 	}
+// }
+
+// func main() {
+// 	test([]emailBill{
+// 		{45},
+// 		{32},
+// 		{43},
+// 		{12},
+// 		{34},
+// 		{54},
+// 	})
+
+// 	test([]emailBill{
+// 		{12},
+// 		{12},
+// 		{976},
+// 		{12},
+// 		{543},
+// 	})
+
+// 	test([]emailBill{
+// 		{743},
+// 		{13},
+// 		{8},
+// 	})
+// }
+
+// ################################################################
+// ANONYMOUS FUNCTIONS
+// Anonymous functions are true to form in that they have no name. We've been using them throughout this chapter, but we haven't really talked about them yet.
+
+// Anonymous functions are useful when defining a function that will only be used once or to create a quick closure.
+
+// // doMath accepts a function that converts one int into another
+// // and a slice of ints. It returns a slice of ints that have been
+// // converted by the passed in function.
+// func doMath(f func(int) int, nums []int) []int {
+// 	var results []int
+// 	for _, n := range nums {
+// 		results = append(results, f(n))
+// 	}
+// 	return results
+// }
+
+// func main() {
+// 	nums := []int{1, 2, 3, 4, 5}
+
+//     // Here we define an anonymous function that doubles an int
+//     // and pass it to doMath
+// 	allNumsDoubled := doMath(func(x int) int {
+// 	    return x + x
+// 	}, nums)
+
+// 	fmt.Println(allNumsDoubled)
+//     // prints:
+//     // [2 4 6 8 10]
+// }
+
+// ===============================
+
+func printReports(messages []string) {
 	// ?
-	sum := 0
-	return func(i int) int {
-		sum += i
-		return sum
+	for _, message := range messages {
+		printCostReport(func(s string) int {
+			return len(s) * 2
+		}, message)
 	}
 }
 
 // don't touch below this line
 
-type emailBill struct {
-	costInPennies int
-}
-
-func test(bills []emailBill) {
+func test(messages []string) {
 	defer fmt.Println("====================================")
-	countAdder, costAdder := adder(), adder()
-	for _, bill := range bills {
-		fmt.Printf("You've sent %d emails and it has cost you %d cents\n", countAdder(1), costAdder(bill.costInPennies))
-	}
+	printReports(messages)
 }
 
 func main() {
-	test([]emailBill{
-		{45},
-		{32},
-		{43},
-		{12},
-		{34},
-		{54},
+	test([]string{
+		"Here's Johnny!",
+		"Go ahead, make my day",
+		"You had me at hello",
+		"There's no place like home",
 	})
 
-	test([]emailBill{
-		{12},
-		{12},
-		{976},
-		{12},
-		{543},
+	test([]string{
+		"Hello, my name is Inigo Montoya. You killed my father. Prepare to die.",
+		"May the Force be with you.",
+		"Show me the money!",
+		"Go ahead, make my day.",
 	})
+}
 
-	test([]emailBill{
-		{743},
-		{13},
-		{8},
-	})
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
 }
