03538e9ac752455ba7e50886a5aa5b8876d4ad71
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-13 08:10:10 +0545

    More on Channels

--- learn/0. Another Path/Concurrency/main.go
+++ learn/0. Another Path/Concurrency/main.go
@@ -62,96 +62,142 @@ import (
 
 // ===============================
 
-func filterOldEmails(emails []email) {
-	isOldChan := make(chan bool)
+// func filterOldEmails(emails []email) {
+// 	isOldChan := make(chan bool)
 
-	go sendIsOld(isOldChan, emails)
+// 	go sendIsOld(isOldChan, emails)
 
-	isOld := <-isOldChan
-	fmt.Println("email 1 is old:", isOld)
-	isOld = <-isOldChan
-	fmt.Println("email 2 is old:", isOld)
-	isOld = <-isOldChan
-	fmt.Println("email 3 is old:", isOld)
-}
+// 	isOld := <-isOldChan
+// 	fmt.Println("email 1 is old:", isOld)
+// 	isOld = <-isOldChan
+// 	fmt.Println("email 2 is old:", isOld)
+// 	isOld = <-isOldChan
+// 	fmt.Println("email 3 is old:", isOld)
+// }
 
-// TEST SUITE -- Don't touch below this line
+// // TEST SUITE -- Don't touch below this line
 
-func sendIsOld(isOldChan chan<- bool, emails []email) {
-	for _, e := range emails {
-		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
-			isOldChan <- true
-			continue
-		}
-		isOldChan <- false
+// func sendIsOld(isOldChan chan<- bool, emails []email) {
+// 	for _, e := range emails {
+// 		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+// 			isOldChan <- true
+// 			continue
+// 		}
+// 		isOldChan <- false
+// 	}
+// }
+
+// type email struct {
+// 	body string
+// 	date time.Time
+// }
+
+// func test(emails []email) {
+// 	filterOldEmails(emails)
+// 	fmt.Println("==========================================")
+// }
+
+// func main() {
+// 	test([]email{
+// 		{
+// 			body: "Are you going to make it?",
+// 			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "I need a break",
+// 			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "What were you thinking?",
+// 			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 	})
+// 	test([]email{
+// 		{
+// 			body: "Yo are you okay?",
+// 			date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "Have you heard of that website Boot.dev?",
+// 			date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "It's awesome honestly.",
+// 			date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 	})
+// 	test([]email{
+// 		{
+// 			body: "Today is the day!",
+// 			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "What do you want for lunch?",
+// 			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "Why are you the way that you are?",
+// 			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 	})
+// 	test([]email{
+// 		{
+// 			body: "Did we do it?",
+// 			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "Letsa Go!",
+// 			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 		{
+// 			body: "Okay...?",
+// 			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
+// 		},
+// 	})
+// }
+
+// ################################################################
+// More on CHANNELS
+// Empty structs are often used as tokens in Go programs. In this context, a token is a unary value. In other words, we don't care what is passed through the channel. We care when and if it is passed.
+
+// We can block and wait until something is sent on a channel using the following syntax
+
+// <-ch
+// This will block until it pops a single item off the channel, then continue, discarding the item.
+
+// ===============================
+
+func waitForDbs(numDBs int, dbChan chan struct{}) {
+	// ?
+	for i := 0; i < numDBs; i++ {
+		<-dbChan
 	}
 }
 
-type email struct {
-	body string
-	date time.Time
-}
+// don't touch below this line
 
-func test(emails []email) {
-	filterOldEmails(emails)
-	fmt.Println("==========================================")
+func test(numDBs int) {
+	dbChan := getDatabasesChannel(numDBs)
+	fmt.Printf("Waiting for %v databases...\n", numDBs)
+	waitForDbs(numDBs, dbChan)
+	time.Sleep(time.Millisecond * 10) // ensure the last print statement happens
+	fmt.Println("All databases are online!")
+	fmt.Println("=====================================")
 }
 
 func main() {
-	test([]email{
-		{
-			body: "Are you going to make it?",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "I need a break",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "What were you thinking?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Yo are you okay?",
-			date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Have you heard of that website Boot.dev?",
-			date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "It's awesome honestly.",
-			date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Today is the day!",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "What do you want for lunch?",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Why are you the way that you are?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Did we do it?",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Letsa Go!",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Okay...?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
+	test(3)
+	test(4)
+	test(5)
+}
+
+func getDatabasesChannel(numDBs int) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		for i := 0; i < numDBs; i++ {
+			ch <- struct{}{}
+			fmt.Printf("Database %v is online\n", i+1)
+		}
+	}()
+	return ch
 }
