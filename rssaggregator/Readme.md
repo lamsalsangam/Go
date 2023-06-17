64c5993d6e854b17d9a75ee722b562e7f89f1626
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-17 07:11:29 +0545

    made the JSOn Respond file

--- rssaggregator/Readme.md
+++ rssaggregator/Readme.md
@@ -1,9 +1,11 @@
 # RSS Aggregator
 
-- Initilize the go.mod file with `go mod init [pathname]` you can replace the pathname with your github repository name
+- Initialize the go.mod file with `go mod init [pathname]` you can replace the pathname with your github repository name
 
 - To add the other modules in your go programme you just need to do `go get [pathname]` in this case the github.com/joho/godotenv as the pathname
 
 - Type `go mod tidy` it will clean up the modules
 
 - You can make the copy locally by doing `go mod vendor`
+
+- To spin up the server you need to install `go get [pathname]` in this case we use github.com/go-chi/cors
--- rssaggregator/main.go
+++ rssaggregator/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/lamsalsangam/Go/tree/master/rssaggregator/vendor/github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -18,5 +18,26 @@ func main() {
 		log.Fatal("PORT is not found in the enviroment")
 	}
 
-	fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
