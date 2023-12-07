9e13488344bc484a972b0a000a151aa394ae23c4
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-12-07 13:28:25 +0545

    made the GetTodos handler which retrieves a list of all ToDo items

---
+++ todo-api/internal/handlers/todo_handler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"todo-api/internal/database"
+)
+
+// GetTodos retrieves a list of all ToDo items
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+	err := database.InitDB()
+	if err != nil {
+		log.Printf("Error initializing the database: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	todos, err := database.GetTodos()
+	if err != nil {
+		log.Printf("Error fetching todos: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, todos)
+}
+
+// respondWithJSON sends a JSON response with the specified status code and data
+func respondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
