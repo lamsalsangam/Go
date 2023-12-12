a2076dd68caf7cc1bbe197d05d6af1c581b0aadc
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-12-12 15:19:20 +0545

    Made the DeleteTodo handler which deletes a ToDo item by ID

--- todo-api/internal/handlers/todo_handler.go
+++ todo-api/internal/handlers/todo_handler.go
@@ -109,6 +109,32 @@ func UpdateTodo(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// DeleteTodo deletes a ToDo item by ID
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	err := database.InitDB()
+	if err != nil {
+		log.Printf("Error initializing the database: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	idStr := extractIDFromURL(r)
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid todo ID", http.StatusInternalServerError)
+		return
+	}
+
+	err = database.DeleteTodoByID(id)
+	if err != nil {
+		log.Printf("Error deleting todo: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 // respondWithJSON sends a JSON response with the specified status code and data
 func respondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
