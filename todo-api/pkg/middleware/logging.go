d0bbb870803bf47bb6fa07bd47790b786d8430f4
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-12-27 08:28:15 +0545

    Made the middleware logging

---
+++ todo-api/pkg/middleware/logging.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// LoggingMiddleware is a simple middleware function for logging requests
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+		log.Printf("Request duration: %v", duration)
+	})
+}
